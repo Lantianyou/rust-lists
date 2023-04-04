32fdd28e73ba8b7fba49e5c2fd16aec0505da031
兰天游 <dtlantianyou@gmail.com>
2023-04-04 21:56:03 +0800

    feat: move linked list to Option

--- src/first.rs
+++ src/first.rs
@@ -1,4 +1,69 @@
-pub enum List {
-    Empty,
-    Elem(i32, List)
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+pub struct List<T> {
+    head: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let new_node = Box::new(Node {
+            elem,
+            next: self.head.take(),
+        });
+
+        self.head = Some(new_node)
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        match self.head.take() {
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.elem
+        });
+    }
+}
+
+mod test {
+    use super::List;
+
+    #[test]
+    fn basic() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+    }
 }
